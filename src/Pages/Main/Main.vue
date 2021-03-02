1cbf7e5771a49f19725f8aba149724b458fdaeef
mine <15859145025@163.com>
2021-03-02 20:57:27 +0800

    init

--- src/Pages/Main/Main.vue
+++ src/Pages/Main/Main.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
+    <Header/>
     Main
   </div>
 </template>
 <script>
+// 引入组件
+import Header from '../../components/Header/Header'
 export default {
-  name: 'Main'
+  name: 'Main',
+  components:{
+    Header
+  }
 }
 </script>
 <style lang="less" rel="stylesheet/less" scoped>
